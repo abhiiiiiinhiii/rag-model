c5c30e7f2dacef37e2b8edbc9026267777fff054
abhiiiiiinhiii <abhi880250@gmail.com>
2025-10-27 12:43:31 +0530

    feedback fix

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 set -e
 
+echo "--- Ensuring log files exist... ---"
+touch chat_history.csv
+touch feedback_log.csv
+touch activity_log.csv
 
 echo "--- Tearing down old container (if it exists)... ---"
 # This command stops and removes the containers defined in the docker-compose.yml file
